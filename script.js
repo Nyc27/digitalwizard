54c7148912fd6b171f463167880389ac59772f0f
Nyc27 <156249960+Nyc27@users.noreply.github.com>
2024-04-20 12:31:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// AUTO PLAY VIDEO
+
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.querySelector('.hero-video');
+    video.play();
+})
+
+
+
+// ABILITA MENU MOBILE
+
 const menuButton = document.querySelector('.nav-toggle');
 const mobileUl = document.querySelector('.nav-ul');
 
@@ -11,26 +22,18 @@ menuButton.addEventListener('click', function () {
 })
 
 
-
 // LINK INTERNO --> CHIUSURA MENU
 
-document.querySelectorAll('.mobile-nav-list a').forEach(link => {
-    link.addEventListener('click', function(event) {
+document.querySelectorAll('ul a').forEach(link => {
+    link.addEventListener('click', function() {
         const href = this.getAttribute('href');
 
-        // Verifica se il link è un ancoraggio interno alla stessa pagina
         if (href.startsWith('#')) {
             const nomeId = href.substring(1);
             const idPulito = document.getElementById(nomeId);
 
-            // Verifica se l'elemento target esiste nella pagina corrente
             if (idPulito) {
-                // Chiudi la navigazione
-                const mobileNav = document.querySelector(".mobile-nav");
-                mobileNav.classList.remove('nav-show');
-
-                const overlay = document.querySelector('.overlay');
-                overlay.classList.remove('overlay-show');
+                mobileUl.classList.remove('ul-visible');
 
                 document.body.classList.remove('no-scroll');
             }
@@ -38,6 +41,27 @@ document.querySelectorAll('.mobile-nav-list a').forEach(link => {
     });
 });
 
+// HERO WORDS CHANGING
+
+const words = ["WEB DESIGN", "LOCAL SEO", "seo","SEM", "WEB MARKETING"];
+const title = document.querySelector('.change-word');
+
+let index = 0;
+
+function changeTitle() {
+    title.classList.add('rotating-out');
+    setTimeout(() => {
+        title.textContent = words[index];
+        title.classList.remove('rotating-out');
+        title.classList.add('rotating-in');
+        setTimeout(() => {
+            title.classList.remove('rotating-in');
+        }, 3000);
+        index = (index + 1) % words.length;
+    }, 500);
+}
+
+setInterval(changeTitle, 3000);
 
 
 
@@ -86,25 +110,3 @@ testimonialMobile.forEach(card => {
         portfText.classList.toggle('show-more')
     })
 })
-
-
-
-const words = ["WEB DESIGN", "LOCAL SEO", "seo","SEM", "WEB MARKETING"];
-const title = document.querySelector('.change-word');
-
-let index = 0;
-
-function changeTitle() {
-    title.classList.add('rotating-out');
-    setTimeout(() => {
-        title.textContent = words[index];
-        title.classList.remove('rotating-out');
-        title.classList.add('rotating-in');
-        setTimeout(() => {
-            title.classList.remove('rotating-in');
-        }, 3000);
-        index = (index + 1) % words.length;
-    }, 500); // Tempo di attesa prima di cambiare la parola
-}
-
-setInterval(changeTitle, 3000); // Cambia il titolo ogni 2 secondi (2000 millisecondi)
